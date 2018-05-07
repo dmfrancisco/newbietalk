b849fb40b429cc65cd767567376d63b8f3779a08
Glitch (smalltalk-dev) <none>
2018-05-07 23:41:06 +0000

    👔🙀 More work on the languages

--- src/demo/Screen1.js
+++ src/demo/Screen1.js
@@ -53,12 +53,12 @@ class Screen1 extends Component {
               </select>
             </div>
 
-            <div className="w-3/4 mx-auto rounded-lg bg-blue-lightest p-6 mb-6">
+            <div className="w-3/4 mx-auto rounded-lg bg-blue-lightest p-6 mb-6 overflow-hidden">
               <h4 className="text-lg italic text-center mb-4 tracking-none">What languages do you speak?</h4>
 
               {[0, 1, 2].map((index) => {
                 let placeholder;
-                if (index === 0) placeholder = "1st language…";
+                if (index === 0) placeholder = "1st language…  (e.g. Portuguese)";
                 if (index === 1) placeholder = "Optional: 2nd language…";
                 if (index === 2) placeholder = "Optional: 3rd language…";
 
@@ -68,12 +68,13 @@ class Screen1 extends Component {
                       type="text"
                       onChange={(e) => profile.setLanguageName(index, e.target.value) }
                       value={profile.state.languages[index].name}
-                      className="flex-1 border-2 px-3 py-2 h-10 rounded m-1 mr-0"
+                      className="flex-1 border-2 px-3 py-2 h-10 rounded m-1"
                       placeholder={placeholder}
                     />
 
                     <select
-                      className="block border-2 h-10 bg-transparent rounded text-xl text-center m-1 mr-0"
+                      className="block border-2 h-10 bg-transparent rounded text-2xl text-center m-1 ml-2"
+                      style={{ width: "4rem" }}
                       onChange={(e) => profile.setLanguageIcon(index, e.target.value)}
                       value={profile.state.languages[index].icon}
                     >
